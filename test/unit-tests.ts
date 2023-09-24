b2471f1116ef83173858db02b21f00596838b38a
Matthe815 <matthew.elswick@matthe815.dev>
2023-09-24 13:17:30 -0400

    tests: Finished unit test implementation

--- test/unit-tests.ts
+++ test/unit-tests.ts
@@ -3,20 +3,23 @@ import Monitors from '../src/utils/monitors'
 import MonitorData from '../src/classes/monitordata'
 
 const client = new Client({ intents: ['Guilds'] })
-const CONFIG = require('./config/config.json')
+const CONFIG = require('../config/config.json')
 
 const testConnectionDetails: MonitorData = {
-  host: 'archipelago.gg',
-  port: 54844,
-  game: 'Ocarina of Time',
-  player: 'Matthew',
-  channel: '1155323368342564924'
+  host: CONFIG.test.host,
+  port: CONFIG.test.port,
+  game: CONFIG.test.game,
+  player: CONFIG.test.player,
+  channel: CONFIG.test.channel
 }
 
 client.login(CONFIG.token)
 
-function start () {
-  Monitors.make(testConnectionDetails, client)
+async function start () {
+  const monitor = await Monitors.make(testConnectionDetails, client)
+  monitor.onJSON({ cmd: 'PrintJSON', data: [{ text: "Matthew (Team #1) tracking Ocarina of Time has joined. Client(0.4.2), ['IgnoreGame', 'Tracker']." }], type: 'Join', team: 0, slot: 1, tags: ['IgnoreGame', 'Tracker'] })
+  monitor.onJSON({ cmd: 'PrintJSON', data: [{ text: '4', type: 'player_id' }, { text: ' sent ' }, { text: '66078', player: 1, flags: 0, type: 'item_id' }, { text: ' to ' }, { text: '1', type: 'player_id' }, { text: ' (' }, { text: '42027', player: 4, type: 'location_id' }, { text: ')' }], type: 'ItemSend', receiving: 1, item: { item: 66078, location: 42027, player: 4, flags: 0 } })
+  monitor.onJSON({ cmd: 'PrintJSON', data: [{ text: "Matthew (Team #1) tracking Ocarina of Time has joined. Client(0.4.2), ['IgnoreGame', 'Tracker']." }], type: 'Part', team: 0, slot: 1 })
 }
 
 start()
