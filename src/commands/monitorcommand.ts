6fb06fa108dd406a4f644e63945414aa1b6aa771
Matthe815 <matthew.elswick@matthe815.dev>
2023-09-24 00:49:15 -0400

    refactor: Added validation to monitor command

--- src/commands/monitorcommand.ts
+++ src/commands/monitorcommand.ts
@@ -1,5 +1,5 @@
 import Command from '../classes/command'
-import { ApplicationCommandOption, ApplicationCommandOptionType, CommandInteraction } from 'discord.js'
+import { ApplicationCommandOption, ApplicationCommandOptionType, ChannelType, CommandInteraction } from 'discord.js'
 import MonitorData from '../classes/monitordata'
 import Monitors from '../utils/monitors'
 
@@ -11,7 +11,7 @@ export default class MonitorCommand extends Command {
     { type: ApplicationCommandOptionType.String, name: 'player', description: 'The player to monitor', required: true },
     { type: ApplicationCommandOptionType.String, name: 'host', description: 'The host to use', required: true },
     { type: ApplicationCommandOptionType.Integer, name: 'port', description: 'The port to use', required: true },
-    { type: ApplicationCommandOptionType.Channel, name: 'channel', description: 'The channel to send messages to', required: true }
+    { type: ApplicationCommandOptionType.Channel, channelTypes: [ChannelType.GuildText], name: 'channel', description: 'The channel to send messages to', required: true }
   ]
 
   constructor (client: any) {
@@ -19,8 +19,30 @@ export default class MonitorCommand extends Command {
     this.client = client
   }
 
+  validate (interaction: CommandInteraction) {
+    // test data 2 for proper URL
+    const host = interaction.options.data[2].value as string
+
+    // regex for domain or IP
+    const hostRegex = /^(?:[a-z0-9]+(?:-[a-z0-9]+)*\.)+[a-z]{2,}$/i
+    if (!hostRegex.test(host)) return false
+
+    // test data 4 for proper channel
+    const channel = interaction.options.data[4].channel
+    if (channel == null) return false
+
+    // Only add to channels in this guild
+    if (interaction.guild?.channels.cache.get(channel.id) == null) return false
+
+    return true
+  }
+
   execute (interaction: CommandInteraction) {
     if (interaction.options.data[4].channel == null) return
+    if (!this.validate(interaction)) {
+      interaction.reply({ content: 'Invalid input. Please try again.', ephemeral: true })
+      return
+    }
 
     // Only allow one monitor per host
     if (Monitors.has(`${interaction.options.data[2].value}:${interaction.options.data[3].value}` as string)) {
